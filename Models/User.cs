4e26ce8aadc3cd623a046666fc88bd4937b3bdec
andrewmalley725 <andrew.alley99@gmail.com>
2023-04-13 09:39:25 -0600

    rescaffolded

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace intex_2023_api.Models
+{
+    public partial class User
+    {
+        public string Email { get; set; } = null!;
+        public string Firstname { get; set; } = null!;
+        public string Lastname { get; set; } = null!;
+        public string Hash { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
